7aee40a08d809e10e343badebf5470c06cbe12e9
Rajnaresh Tummala <rajnaresh.t@coppermobile.com>
2019-12-06 20:28:53 -0500

    function basics

--- scripts.js
+++ scripts.js
@@ -1,16 +1,18 @@
-var b = 20;
-console.log(b);
-
-var c = 20.5;
-console.log(c);
-
-var d = "Mythri";
-console.log(d);
-
-var isEmpty = true;
-console.log(isEmpty);
+/* functin defnition*/
 
 
-console.log(g);
+/*5!=5*4*3*2*1 
+4!=*4*3*2*1 
+3!=*3*2*1 
+2!=2*1
+1!=1
+*/
 
 
+ var substrct = function(a){
+     return function(b){
+         return a-b;
+     }
+ }
+ var result = substrct(6)(2);
+ console.log(result);
